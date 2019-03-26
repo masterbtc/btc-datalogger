9a2ee651323b5ee57f9751b834bec9870ece10dd
Roshan Prabhakar <roshancvp@gmail.com>
2019-03-25 23:01:16 -0700

    Fix bugs

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const cron = require('node-cron')
 const fs = require('fs')
 const ex = new ccxt.binance()
 
+fs.mkdir(`${__dirname}/trades`, {recursive: true}, e => console.log)
+fs.mkdir(`${__dirname}/ob`, {recursive: true}, e => console.log)
+
+/* Trades - Global variables */
+let lastId = null
+let q = []
+let count = 0 
+
 /* Trades - CRON Job */
 cron.schedule('0,15,45 * * * * *', () => {
-  const Q_MAX = 500
-  const path = `${__dirname}/trades`
-  let lastId = null
-  let q = []
-  let count = 0 
-
-  fs.mkdir(path, {recursive: true}, e => console.log)
-  
+  const Q_MAX = 500  
   ex.fetchTrades('BTC/USDT')
   .then(trades => {
     if (lastId !== null) {
@@ -22,7 +23,9 @@ cron.schedule('0,15,45 * * * * *', () => {
       for (let i = trades.length - 1; i > -1; i--) {
         if (trades[i].id === lastId) {
           newTrades = trades.slice(i + 1)
+          console.log('new length',newTrades.length)
           lastId = newTrades[newTrades.length - 1].id
+          console.log('lastId 2', lastId)
           q.push(...newTrades)
           break;
         }
@@ -31,14 +34,15 @@ cron.schedule('0,15,45 * * * * *', () => {
       //first push
       q.push(...trades)
       lastId = trades[trades.length - 1].id
+      console.log('lastId 1', lastId)
     }
     
-    console.log('q length', q.length)
+    console.log(`${count}: qsize =`, q.length)
     // write to file
     if (q.length >= Q_MAX) {
       let toWrite = q.slice(0, Q_MAX)
       q = q.slice(Q_MAX)
-      fs.writeFileSync(`${path}/${count}.json`, JSON.stringify(toWrite))
+      fs.writeFileSync(`${__dirname}/trades/${count}.json`, JSON.stringify(toWrite))
       count += 1
     }
   })
@@ -46,8 +50,6 @@ cron.schedule('0,15,45 * * * * *', () => {
 })
 
 cron.schedule('0,10,20,30,40,50 * * * * *', () => {
-  fs.mkdir(`${__dirname}/ob`, {recursive: true}, e => console.log)
-  
   ex.fetchOrderBook('BTC/USDT')
   .then(ob => {
     fs.writeFileSync(`${__dirname}/ob/${new Date().getTime()}.json`, JSON.stringify(ob))
